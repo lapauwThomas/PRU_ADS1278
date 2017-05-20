8eafe534298ed8c173bda75f6fb833e2ef634695
Thomas Lapauw <tlapauw@vub.be>
2017-05-20 23:18:37 +0200

    added edge detection

---
+++ PRU_ADS1278/Debug/main.d
@@ -0,0 +1,33 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../resource_table_0.h
+main.obj: C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stddef.h
+main.obj: C:/ti2/pru-software-support-package/include/rsc_types.h
+main.obj: C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdint.h
+main.obj: C:/ti2/pru-software-support-package/include/pru_types.h
+main.obj: C:/ti2/pru-software-support-package/include/pru_virtio_ids.h
+main.obj: C:/ti2/pru-software-support-package/include/am335x/pru_cfg.h
+main.obj: C:/ti2/pru-software-support-package/include/am335x/pru_intc.h
+main.obj: C:/ti2/pru-software-support-package/include/pru_rpmsg.h
+main.obj: C:/ti2/pru-software-support-package/include/pru_virtqueue.h
+main.obj: C:/ti2/pru-software-support-package/include/pru_virtio_ring.h
+main.obj: C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdio.h
+main.obj: C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/linkage.h
+main.obj: C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdarg.h
+
+../main.c: 
+../resource_table_0.h: 
+C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stddef.h: 
+C:/ti2/pru-software-support-package/include/rsc_types.h: 
+C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdint.h: 
+C:/ti2/pru-software-support-package/include/pru_types.h: 
+C:/ti2/pru-software-support-package/include/pru_virtio_ids.h: 
+C:/ti2/pru-software-support-package/include/am335x/pru_cfg.h: 
+C:/ti2/pru-software-support-package/include/am335x/pru_intc.h: 
+C:/ti2/pru-software-support-package/include/pru_rpmsg.h: 
+C:/ti2/pru-software-support-package/include/pru_virtqueue.h: 
+C:/ti2/pru-software-support-package/include/pru_virtio_ring.h: 
+C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdio.h: 
+C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/linkage.h: 
+C:/ti2/ccsv7/tools/compiler/ti-cgt-pru_2.1.4/include/stdarg.h: 
